8ddaee6bf318753d188aa665ece08a2e11c45da9
Ryan Schuft <ryan.schuft@gmail.com>
2024-09-06 16:37:59 -0400

    Added annotation and path color configuration

--- YoloSampler/__init__.py
+++ YoloSampler/__init__.py
@@ -1,7 +1,6 @@
 __all__ = [
-    'draw_bounding_boxes',
+    'overlay_tracking_details',
     'open_webcam',
-    'overlay_bounding_boxes',
     'read_frame',
     'record_object_tracking',
     'release_capture',
@@ -19,18 +18,15 @@ import cv2
 import numpy as np
 from ultralytics import YOLO
 
-# Overlay last recorded bounding boxes on the current frame
-def draw_bounding_boxes(frame, prev_boxes, track_history):
+# Overlay last recorded bounding boxes on the current frame and the path of the object
+def overlay_tracking_details(frame, prev_boxes, track_history, annotation_color = (0, 255, 0), path_color = (230, 230, 230)):
     for (x1, y1, x2, y2, label, confidence, track_id) in prev_boxes:
-        # Draw bounding box
-        cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
-        # Put label, confidence, and tracking ID
+        cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), annotation_color, 2)
         label_text = f"{label} {confidence:.2f} {track_id}"
-        cv2.putText(frame, label_text, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        # Draw the tracking lines
-        track = track_history[track_id]
-        points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
-        cv2.polylines(frame, [points], isClosed=False, color=(230, 230, 230), thickness=7)
+        cv2.putText(frame, label_text, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, annotation_color, 2)
+        if track_id is not None:
+            points = [np.hstack(track_history[track_id]).astype(np.int32).reshape((-1, 1, 2))]  # for later: can we move np.int32 into the hstack call?
+            cv2.polylines(frame, points, isClosed=False, color=path_color, thickness=7)
 
 # Read tracked info out of rbox and store it in prev_boxes and track_history
 def record_object_tracking(labels, prev_boxes, track_history, rbox, max_track_length = 30):
@@ -75,42 +71,32 @@ def track_frame(tracking_model, frame, prev_boxes, track_history, max_track_leng
         for rbox in r.boxes:
             record_object_tracking(tracking_model.names, prev_boxes, track_history, rbox, max_track_length)
 
-# Overlay previous bounding boxes on the current frame
-def overlay_bounding_boxes(frame, prev_boxes, track_history, frame_label = 'Object Tracking'):
-    for (x1, y1, x2, y2, label, confidence, track_id) in prev_boxes:
-        draw_bounding_boxes(frame, prev_boxes, track_history)
-    cv2.imshow(frame_label, frame)
-
 # Track objects in the video stream, annotate the frame, and display the video
-def track_video_stream(cap, tracking_model, detection_interval = 0.25, max_track_length = 30, frame_label = 'Object Tracking'):
+def track_video_stream(cap, tracking_model, detection_interval = 0.25, max_track_length = 30, frame_label = 'Object Tracking', annotation_color = (0, 255, 0), path_color = (230, 230, 230)):
     last_detection_time = 0  # Updated with the current time after each detection
     prev_boxes = []  # Format: [(x1, y1, x2, y2, label, confidence, track_id), ...]
     track_history = defaultdict(lambda: [])  # Format: {track_id: [(x1, y1), (x2, y2), ...]}
     while cap.isOpened():
         frame = read_frame(cap)  # Capture frame-by-frame
-
         # Run object tracking every N ms
         current_time = time.time()
         if current_time - last_detection_time >= detection_interval:
             prev_boxes = []
             track_frame(tracking_model, frame, prev_boxes, track_history, max_track_length)
             last_detection_time = current_time
-
         # Overlay previous bounding boxes on the current frame
-        overlay_bounding_boxes(frame, prev_boxes, track_history, frame_label)
-
+        overlay_tracking_details(frame, prev_boxes, track_history, annotation_color, path_color)
+        cv2.imshow(frame_label, frame)
         # Break the loop if the 'ESC' key is pressed
         if cv2.waitKey(1) == 27:  # 27 is the ASCII value for the 'ESC' key
             break
-
     release_capture(cap)
 
-
 # Load YOLO model, open webcam and run object detection
-def track(tracking_model_name = 'yolov8x.pt', device_index = 0, width = 3840, height = 2160, detection_interval = 0.25, max_track_length = 30, frame_label = 'Object Tracking'):
+def track(tracking_model_name = 'yolov8x.pt', device_index = 0, width = 3840, height = 2160, detection_interval = 0.25, max_track_length = 30, frame_label = 'Object Tracking', annotation_color = (0, 255, 0), path_color = (230, 230, 230)):
     tracking_model = YOLO(tracking_model_name)
     cap = open_webcam(device_index, width, height)
-    track_video_stream(cap, tracking_model, detection_interval, max_track_length, frame_label)
+    track_video_stream(cap, tracking_model, detection_interval, max_track_length, frame_label, annotation_color, path_color)
 
 # Main entry point
 if __name__ == "__main__":
--- yolo-sampler.py
+++ yolo-sampler.py
@@ -10,7 +10,9 @@ detection_interval = config['detection_interval'] or 0.25  # Delay between detec
 max_track_length = config['max_track_length'] or 30  # Maximum number of points to keep in the track history
 tracking_model_name = config['tracking_model_name'] or 'yolov8x.pt' # YOLO model name
 frame_label = config['frame_label'] or 'Object Tracking'  # Label for the window frame
+annotation_color = config['annotation_color'] or (0, 255, 0)  # Bounding box and label color
+path_color = config['path_color'] or (230, 230, 230)  # Tracking path color
 
 # Main entry point
 if __name__ == "__main__":
-    track(tracking_model_name, device_index, width, height, detection_interval, max_track_length, frame_label)
+    track(tracking_model_name, device_index, width, height, detection_interval, max_track_length, frame_label, annotation_color, path_color)
